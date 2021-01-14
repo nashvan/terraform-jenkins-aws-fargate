5964b1c64d7ae1e6f123c2ed62b7f232a6d9ba63
nashwan <nashwan.bilal@gmail.com>
2021-01-15 00:00:02 +1100

    added modules

---
+++ account_config.tf
@@ -0,0 +1,30 @@
+locals {
+  # account_id              = data.aws_caller_identity.current.account_id
+  app_name                = "jenkins"
+  application_id          = "APID001"
+  cost_centre             = "CC001"
+  iam_name_prefix         = "abc"
+  service_name            = "krd"
+  disable_api_termination = "false"
+  # alb_access_logs_bucket  = "${local.account_id}-alb-log-bucket"
+  proxy_host              = ""
+  container_port          = 8080
+  JenkinsJNLPPort         = 50000
+  prefix_name             = "${local.service_name}-${local.app_name}-${var.environment}"
+
+  account_configs = {
+    develop = {
+      record_set_name = "cmcloudlab396.info."
+    }
+
+    sit = {
+      record_set_name = "cmcloudlab872.info."
+    }
+
+    prod = {
+      record_set_name = "cmcloudlab1725.info."
+    }
+  }
+
+  account_config = local.account_configs[var.environment]
+}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+aws_access_key = "AKIA4JN5DTNMHMN3JN5Y"
+aws_secret_key = "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"
